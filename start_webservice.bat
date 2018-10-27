15d0d0336576b7e015862e4a2ecc02aabde9ec5c
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-10-27 12:24:14 +0100

    trying to make the git push heroku master work by changing requirements.txt

--- start_webservice.bat
+++ start_webservice.bat
@@ -7,9 +7,13 @@ REM cd <my git repository>
 REM heroku create
 REM heroku login
 REM git remote -v
+
+REM (optional) make sure that it will be pushed to the right heroku-repository (here we want it to be pushed to "thawing-inlet-61413")
 REM heroku git:remote -a thawing-inlet-61413
 git push heroku master
 REM
 
 REM setting up new webserver:
 pip install -r requirements.txt
+
+d281a794-83ac-4173-88a2-6ee7165dc25b
