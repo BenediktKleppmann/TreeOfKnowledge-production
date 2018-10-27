465bcaa724613e44676950045c5ec7c168853d8f
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-10-27 11:29:44 +0100

    following hellowebapp instructions

--- start_webservice.bat
+++ start_webservice.bat
@@ -1,3 +1,15 @@
 cd "C:\Users\owner\Documents\1 projects\2015-01-01 !! Tree of Knowledge\webservice"
 venv\Scripts\activate
 python manage.py runserver
+
+REM deploy to heroku
+REM cd <my git repository>
+REM heroku create
+REM heroku login
+REM git remote -v
+REM heroku git:remote -a thawing-inlet-61413
+git push heroku master
+REM
+
+REM setting up new webserver:
+pip install -r requirements.txt
