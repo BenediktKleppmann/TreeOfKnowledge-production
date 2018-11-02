e37c866e74849debc03d60125e4e5e18f3640ddd
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-11-02 11:24:51 +0000

    bug fix in models.py

--- collection/models.py
+++ collection/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Newsletter_subscriber(models.Model):
 	email = models.EmailField(unique=True)
 	first_name = models.CharField(max_length=255)
-	is_templar = models.BooleanField(default=False)
-	is_alchemist = models.BooleanField(default=False)
-	is_archivist = models.BooleanField(default=False)
+	# is_templar = models.BooleanField(default=False)
+	# is_alchemist = models.BooleanField(default=False)
+	# is_archivist = models.BooleanField(default=False)
 	
 
 
