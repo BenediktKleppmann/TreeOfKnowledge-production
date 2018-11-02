024a98114e4b93257c0692b6fb30d92656995e02
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-11-02 11:20:10 +0000

    layout changes to the landing page

--- collection/models.py
+++ collection/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Newsletter_subscriber(models.Model):
 	email = models.EmailField(unique=True)
 	first_name = models.CharField(max_length=255)
+	is_templar = models.BooleanField(default=False)
+	is_alchemist = models.BooleanField(default=False)
+	is_archivist = models.BooleanField(default=False)
+	
 
 
 class User(models.Model):
