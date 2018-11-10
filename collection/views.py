104a0088bfdb1f2de81c2c0edd4c4b181fe77a24
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-11-10 12:52:15 +0000

    putting all the pages together - the original main-page is now the about page. some formatting still has to be done.

--- collection/views.py
+++ collection/views.py
@@ -3,11 +3,14 @@ from collection.models import Newsletter_subscriber
 from collection.forms import Subscriber_preferencesForm
 
 # Create your views here.
-def index(request):
-	return render(request, 'index.html')
-
 def landing_page(request):
 	return render(request, 'landing_page.html')
+
+def about(request):
+	return render(request, 'about.html')
+
+def subscribe(request):
+	return render(request, 'subscribe.html')
 
 def contact(request):
 	return render(request, 'contact.html')
@@ -15,11 +18,6 @@ def contact(request):
 def newsletter_subscribers(request):
 	newsletter_subscribers = Newsletter_subscriber.objects.all().order_by('email')
 	return render(request, 'newsletter_subscribers.html', {'newsletter_subscribers': newsletter_subscribers,})
-
-# def subscriber_page(request, userid):
-# 	subscriber = Newsletter_subscriber.objects.get(userid=userid)	
-# 	return render (request, 'subscribers/subscriber_page.html', {'subscriber':subscriber})
-
 
 def subscriber_page(request, userid):
 	subscriber = Newsletter_subscriber.objects.get(userid=userid)
--- webservice/urls.py
+++ webservice/urls.py
@@ -7,8 +7,9 @@ from django.urls import path
 
 
 urlpatterns = [
-	url(r'^$', views.index, name='home'),
-	url(r'^landing_page/$', views.landing_page, name='landing_page'),
+	url(r'^$', views.landing_page, name='landing_page'),
+	url(r'^about/$', views.about, name='about'),
+	url(r'^subscribe/$', views.subscribe, name='subscribe'),
 	url(r'^contact/$', views.contact, name='contact'),
 	url(r'^subscribers/$', views.newsletter_subscribers, name='subscribers'),
 	# url(r'^subscriber_home'),
