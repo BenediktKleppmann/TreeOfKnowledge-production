36f2b352288381d90336bc658151fd731b9499f3
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-11-13 21:41:12 +0000

    adding classes Simulation_model and Uploaded_dataset to models.py. These data tables have an automatically created slug and created-timestamp.

--- collection/admin.py
+++ collection/admin.py
@@ -2,14 +2,28 @@ from django.contrib import admin
 
 # import the models
 from collection.models import Newsletter_subscriber
+from collection.models import Simulation_model
+from collection.models import Uploaded_dataset
 
 # automated email_hash creation for Newsletter subscribers
 class Newsletter_subscriberAdmin(admin.ModelAdmin):
 	model = Newsletter_subscriber
 	list_display = ('email', 'userid', 'first_name','is_templar', 'is_alchemist', 'is_scholar', 'created', 'updated')
 	# prepopulated_fields = {'userid': ('email',), 'created': ('email',),'updated': ('email',),}
+
+class Simulation_modelAdmin(admin.ModelAdmin):
+	model = Simulation_model
+	list_display = ('name', 'description', 'slug', 'created', 'updated')
+	prepopulated_fields = {'slug': ('name',)}
+
+class Uploaded_datasetAdmin(admin.ModelAdmin):
+	model = Simulation_model
+	list_display = ('name', 'description', 'slug', 'uploaded')
+	prepopulated_fields = {'slug': ('name',)}
 	
 	
 
 # register the models
 admin.site.register(Newsletter_subscriber, Newsletter_subscriberAdmin)
+admin.site.register(Simulation_model, Simulation_modelAdmin)
+admin.site.register(Uploaded_dataset, Uploaded_datasetAdmin)
--- collection/models.py
+++ collection/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 import datetime
 import hashlib
 
@@ -14,7 +15,6 @@ class Newsletter_subscriber(models.Model):
 	updated = models.DateTimeField(editable=False)
 
 	def save(self):
-
 		# set the userid to be the md5-hash of the email
 		email_string = self.email.encode('utf-8')
 		self.userid = int(hashlib.sha1(email_string).hexdigest(), 16) % (10 ** 8)
@@ -24,10 +24,48 @@ class Newsletter_subscriber(models.Model):
 		self.updated = datetime.datetime.today()
 		super(Newsletter_subscriber, self).save()
 
-	
 
 
 class User(models.Model):
 	email = models.EmailField(unique=True)
 	username = models.CharField(max_length=255, unique=True)
 	password = models.CharField(max_length=255)
+	created = models.DateTimeField(editable=False)
+	updated = models.DateTimeField(editable=False)
+	def save(self):
+		if not self.id:
+			self.created = datetime.datetime.today()
+		self.updated = datetime.datetime.today()
+		super(Simulation_model, self).save()
+
+
+class Simulation_model(models.Model):
+	name = models.CharField(max_length=255)
+	slug = models.SlugField(unique=True)
+	description = models.TextField()
+	specification = JSONField()
+	created = models.DateTimeField(editable=False)
+	updated = models.DateTimeField(editable=False)
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True,)
+	def save(self):
+		if not self.id:
+			self.created = datetime.datetime.today()
+		self.updated = datetime.datetime.today()
+		super(Simulation_model, self).save()
+
+
+
+class Uploaded_dataset(models.Model):
+	name = models.CharField(max_length=255)
+	slug = models.SlugField(unique=True)
+	description = models.TextField()
+	specification = JSONField()
+	uploaded = models.DateTimeField(editable=False)
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True,)
+	def save(self):
+		self.uploaded = datetime.datetime.today()
+		super(Uploaded_dataset, self).save()
+
+
+
+
