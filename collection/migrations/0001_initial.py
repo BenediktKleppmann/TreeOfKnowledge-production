4b56d74d16fe860fb161a25284f08c3ba7a59ac2
Benedikt Kleppmann <benedikt@kleppmann.de>
2019-01-08 15:33:34 +0000

    still fiddling around with the migrations

--- collection/migrations/0001_initial.py
+++ collection/migrations/0001_initial.py
@@ -46,7 +46,6 @@ class Migration(migrations.Migration):
                 ('specification', models.TextField()),
                 ('created', models.DateTimeField(editable=False)),
                 ('updated', models.DateTimeField(editable=False)),
-                ('is_private', models.BooleanField(default=False)),
                 ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
             ],
         ),
