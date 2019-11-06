4cd3ae790a20c5e012bc6773d7281d4448db991b
owner <benedikt@kleppmann.de>
2019-11-06 11:58:14 +0100

    trying to do some bug fixing related to query_datapoints.get_data_from_related_objects

--- collection/functions/query_datapoints.py
+++ collection/functions/query_datapoints.py
@@ -495,11 +495,9 @@ def filter_and_make_df_from_datapoints(object_type_id, object_ids, filter_facts,
         print('========================================= TESTING =========================================')
 
         unfiltered_object_ids = cursor.execute('SELECT * FROM pg_catalog.pg_tables')
-        cursor.execute(query_string)
         print(str([str(entry) for entry in cursor.fetchall()]))
 
         unfiltered_object_ids = cursor.execute('SELECT object_id FROM unfiltered_object_ids')
-        cursor.execute(query_string)
         print(str([str(entry) for entry in cursor.fetchall()]))
         print('===========================================================================================')
 
