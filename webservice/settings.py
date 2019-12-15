e28436ac49a0f23b8e84312c8a7caf232b3b48de
owner <benedikt@kleppmann.de>
2019-12-15 18:46:47 +0100

    tiny change to settings.py STATIC_URL

--- webservice/settings.py
+++ webservice/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'collection/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
