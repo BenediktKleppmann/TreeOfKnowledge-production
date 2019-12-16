4dc0c3e06321c15d8782b7caa3dc1ea257c26831
owner <benedikt@kleppmann.de>
2019-12-16 13:24:25 +0100

    trying to setup a postgresql database for AWS EB

--- webservice/settings_production.py
+++ webservice/settings_production.py
@@ -1,23 +1,37 @@
-# # Inherit from standard settings file for default
-# from webservice.settings import *
-# import os
+# Inherit from standard settings file for default
+from webservice.settings import *
+import os
 
-# # Everything below will override our standard settings:
+# Everything below will override our standard settings:
 
-# # Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config()
-# DB_CONNECTION_URL = os.environ['DATABASE_URL']
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
 
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+	DATABASES['default'] = dj_database_url.config()
 
-# # Allow all host headers
-# ALLOWED_HOSTS  = ['*']
+DB_CONNECTION_URL = os.environ['DATABASE_URL']
 
-# # Set debug to False
-# DEBUG = False
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# # Static asset configuration
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# Allow all host headers
+ALLOWED_HOSTS  = ['*']
+
+# Set debug to False
+DEBUG = False
+
+# Static asset configuration
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
