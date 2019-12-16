dd6b4797d257dff23ff944aac84b0ab74737b0ee
owner <benedikt@kleppmann.de>
2019-12-16 11:22:24 +0100

    some testing related to the Users on EB

--- webservice/settings_production.py
+++ webservice/settings_production.py
@@ -1,23 +1,23 @@
-# Inherit from standard settings file for default
-from webservice.settings import *
-import os
+# # Inherit from standard settings file for default
+# from webservice.settings import *
+# import os
 
-# Everything below will override our standard settings:
+# # Everything below will override our standard settings:
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-DB_CONNECTION_URL = os.environ['DATABASE_URL']
+# # Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config()
+# DB_CONNECTION_URL = os.environ['DATABASE_URL']
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS  = ['*']
+# # Allow all host headers
+# ALLOWED_HOSTS  = ['*']
 
-# Set debug to False
-DEBUG = False
+# # Set debug to False
+# DEBUG = False
 
-# Static asset configuration
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# # Static asset configuration
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
