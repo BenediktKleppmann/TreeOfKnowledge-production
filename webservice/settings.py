e5c0750e85a80b678a285df554b3cbcbd5cbc699
owner <benedikt@kleppmann.de>
2019-12-17 11:00:01 +0100

    trying to upload the remaining data to the EB database

--- webservice/settings.py
+++ webservice/settings.py
@@ -192,7 +192,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'static'),
+	os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 REGISTRATION_OPEN = True
