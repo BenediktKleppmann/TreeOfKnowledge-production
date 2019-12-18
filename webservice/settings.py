86993c5a0dd7670cfdbf882be2a9f86c78fe2557
owner <benedikt@kleppmann.de>
2019-12-18 14:53:40 +0100

    trying to upload the data to the remaining table (data_points)

--- webservice/settings.py
+++ webservice/settings.py
@@ -192,7 +192,7 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = 'collection/static'
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'collection/static'),
+	os.path.join(BASE_DIR, 'static'),
 )
 
 REGISTRATION_OPEN = True
