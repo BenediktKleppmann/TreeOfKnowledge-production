edbff44427e57354fee779d6c228892a650d673c
owner <benedikt@kleppmann.de>
2020-06-29 17:31:34 +0200

    playing around with boto3.sqs

--- collection/views.py
+++ collection/views.py
@@ -2029,7 +2029,7 @@ def test_page2(request):
     sqs = boto3.client('sqs', region_name='eu-central-1')
 
     # queue = sqs.get_queue_by_name(QueueName='awseb-e-8ps6q6m3je-stack-AWSEBWorkerQueue-1RIUDLVL1OCH2')
-    queue_url = sqs.get_queue_url(QueueName='awseb-e-8ps6q6m3je-stack-AWSEBWorkerQueue-1RIUDLVL1OCH2')
+    queue_url = sqs.get_queue_url(QueueName='Treeofknowledge-queue')
 
     # queue_url = 'https://sqs.eu-central-1.amazonaws.com/662304246363/awseb-e-8ps6q6m3je-stack-AWSEBWorkerQueue-1RIUDLVL1OCH2'
     # response = queue.send_message(MessageBody='world')
