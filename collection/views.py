ed2bb553ea01ffd756476cfffe36f8822292095b
owner <benedikt@kleppmann.de>
2020-06-30 16:35:33 +0200

    changes to test_page2 - testing the sqs queue

--- collection/views.py
+++ collection/views.py
@@ -1997,11 +1997,10 @@ def test_page1(request):
     import psycopg2
     connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="postgres")
     cursor = connection.cursor()
-    cursor.execute('''SELECT * 
-                FROM collection_likelihood_fuction
-                WHERE simulation_id = 416
-                 AND object_number = 1
-                  AND parameter_id = 60;
+    cursor.execute('''SELECT table_name
+                      FROM information_schema.tables
+                      WHERE table_schema='public'
+                       AND table_type='BASE TABLE';
                     ''')
 
     mobile_records = cursor.fetchall() 
