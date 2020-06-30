35c6c13f9b016a2441b09e99a77dec2a026ae483
owner <benedikt@kleppmann.de>
2020-06-30 15:42:53 +0200

    changes to test_page2 - testing the sqs queue

--- collection/views.py
+++ collection/views.py
@@ -1995,7 +1995,7 @@ def upload_file(request):
 # ==================
 def test_page1(request):
     import psycopg2
-    connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="aa1pbfgh471h051")
+    connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="postgres_db")
     cursor = connection.cursor()
     cursor.execute('''SELECT * 
                 FROM collection_likelihood_fuction
