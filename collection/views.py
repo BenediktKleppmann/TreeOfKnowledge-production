6a8e4a7c20eb7142df414622de19232a41eb67ad
owner <benedikt@kleppmann.de>
2020-07-01 09:45:42 +0200

    changes to test_page2 - testing the sqs queue

--- collection/views.py
+++ collection/views.py
@@ -1997,7 +1997,7 @@ def test_page1(request):
     import psycopg2
     connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="postgres")
     cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE tested_simulation_parameters ( 
+    cursor.execute('''CREATE TABLE "tested_simulation_parameters" ( 
                         simulation_id       integer NOT NULL,
                         run                 integer NOT NULL,
                         parameter_value     real NOT NULL,
@@ -2011,7 +2011,7 @@ def test_page1(request):
 
     cursor.execute('''SELECT EXISTS (
                         SELECT * 
-                        FROM tested_simulation_parameters
+                        FROM "tested_simulation_parameters"
                        );
                     ''')
 
@@ -2069,7 +2069,7 @@ def test_page3(request):
 
     cursor.execute('''SELECT EXISTS (
                         SELECT * 
-                        FROM tested_simulation_parameters
+                        FROM "tested_simulation_parameters"
                        );
                     ''')
 
