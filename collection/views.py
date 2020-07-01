59da222205dfbb3413aa64aa0a1852992aa21f4b
owner <benedikt@kleppmann.de>
2020-07-01 10:04:33 +0200

    changes to test_page2 - testing the sqs queue

--- collection/views.py
+++ collection/views.py
@@ -2025,6 +2025,7 @@ def test_page1(request):
 
 
 def test_page2(request):
+    connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="postgres")
     cursor = connection.cursor()
     cursor.execute('''select * from information_schema.tables where table_schema NOT IN (‘pg_catalog’, ‘information_schema’)
                     ''')
@@ -2052,6 +2053,7 @@ def test_page3(request):
 
 
     # import psycopg2
+    connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="postgres")
     cursor = connection.cursor()
     cursor.execute('''SELECT EXISTS (
                         SELECT * 
