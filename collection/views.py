fe2e9b3eb6eb4bf5f329972102254e99fd5e5b76
owner <benedikt@kleppmann.de>
2020-07-09 12:09:05 +0200

    testing how the workers can get simulation data from the database

--- collection/views.py
+++ collection/views.py
@@ -2067,7 +2067,7 @@ def test_page2(request):
 def test_page3(request):
 
     # return redirect('analyse_simulation', simulation_id=420)
-    connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="postgres")
+    connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="collection")
     cursor = connection.cursor()
     cursor.execute('''select validation_data from collection_simulation_model;
                     ''')
