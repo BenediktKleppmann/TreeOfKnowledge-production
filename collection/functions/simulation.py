270d4c6f4b4cf0ccb4d0a1080b741395b2092c8c
owner <benedikt@kleppmann.de>
2020-07-29 14:20:03 +0200

    changed the maximal execution time for learning parameters

--- collection/functions/simulation.py
+++ collection/functions/simulation.py
@@ -520,10 +520,11 @@ class Simulator:
                     response = sqs.send_message(QueueUrl= queue_url, MessageBody=json.dumps(simulation_parameters))
 
                 result_checking_start_time = time.time()
+                maximal_execution_time = max(self.nb_of_tested_parameters * 15, 300)
                 connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="ebdb")
                 cursor = connection.cursor()
                 all_simulation_results = []
-                while (time.time() - result_checking_start_time < 36000):
+                while (time.time() - result_checking_start_time < maximal_execution_time):
 
                     time.sleep(1)
                     cursor.execute('''SELECT simulation_id, run_number, batch_number, priors_dict, simulation_results FROM tested_simulation_parameters WHERE simulation_id=%s AND run_number=%s;''' % (self.simulation_id, self.run_number))
